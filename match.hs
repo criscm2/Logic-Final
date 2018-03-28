e40be4c374a0e7e03c37bd557a241821049d0a3a
wheatwizard <olivee3@rpi.edu>
2018-03-28 13:45:27 -0400

    Fixed self referential statements

--- match.hs
+++ match.hs
@@ -12,8 +12,7 @@ instance Show Equivalence where
  show (Equivalence chr st) = '{':chr:"} => "++show st
 
 contains :: Statement -> Statement -> Bool
-contains a b
- | a == b = True
+contains a b | a == b = True
 contains (If a1 a2) b   = contains a1 b || contains a2 b
 contains (Neg a) b      = contains a b
 contains (Atom _) b     = False
@@ -27,20 +26,30 @@ replace (If a b) equiv = If (replace a equiv) (replace b equiv)
 replace (Neg a) equiv  = Neg $ replace a equiv
 replace a@(Atom _) _   = a
 
+
 equivalence :: Statement -> Statement -> Maybe [Equivalence]
-equivalence (Neg a) (Neg b) = equivalence a b
-equivalence (Atom a) (Atom b)
- | a == b    = Just []
- | otherwise = Nothing
-equivalence (Variable a) st
- | contains st (Variable a) = Nothing
- | otherwise = Just [Equivalence a st]
-equivalence st (Variable a)
- | contains st (Variable a) = Nothing
- | otherwise = Just [Equivalence a st]
+
 equivalence (If a b) (If c d)
  | (Just equivList) <- equivalence a c = fmap (equivList ++) $ equivalence (foldl replace b equivList) (foldl replace d equivList)
  | otherwise = Nothing
+
+equivalence (Neg a) (Neg b) = equivalence a b
+
+equivalence (Atom a) (Atom b)
+ | a == b    = Just []
+ | otherwise = Nothing
+
+equivalence (Variable a) (Variable b) | a == b = Just []
+
+equivalence (Variable a) st
+ | contains st (Variable a) = Nothing
+ | otherwise = Just [Equivalence a st]
+
+equivalence st (Variable a)
+ | contains st (Variable a) = Nothing
+ | otherwise = Just [Equivalence a st]
+
 equivalence _ _ = Nothing
 
-main=print$equivalence (If (Atom 'A') (Variable 'Ω')) (If (Variable '∆') (Atom 'B'))
+
+main = print $ equivalence (Variable 'π') (Variable 'Ω')
